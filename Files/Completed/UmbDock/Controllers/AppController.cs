148a75173f4211701156faa8f3d749838d489c66
Carl Sargunar <carl@sargunar.com>
2022-05-22 12:18:19 +0100

    External Umbraco Models

---
+++ Files/Completed/UmbDock/Controllers/AppController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace UmbDock.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/application/[action]")]
+    public class AppController : UmbracoApiController
+    {
+
+        public AppController()
+        {
+            
+        }
+
+        public IEnumerable<string> GetBlogSummaries()
+        {
+            // Routes to /Umbraco/Api/App/GetBlogSummaries
+            return new[] { "Table", "Chair", "Desk", "Computer" };
+        }
+
+    }
+}
---
+++ Files/Completed/notes.md
@@ -0,0 +1,2 @@
+https://24days.in/umbraco-cms/2021/heartcore-christmas-bingo/
+
